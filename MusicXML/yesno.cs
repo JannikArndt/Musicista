75c30da655a494ef68d1c31a586d1de647c8a26b
Jannik Arndt <Jannik@JannikArndt.de>
2014-05-02 14:12:17 +0200

    Missing file

---
+++ MusicXML/yesno.cs
@@ -0,0 +1,16 @@
+using System;
+using System.CodeDom.Compiler;
+using System.Xml.Serialization;
+
+
+[GeneratedCode("xsd", "4.0.30319.33440")]
+[Serializable]
+[XmlType(TypeName = "yes-no")]
+public enum yesno
+{
+    
+    yes,
+
+    
+    no,
+}

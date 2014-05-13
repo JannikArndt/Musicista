5171b4a10802a8ee095eea628f27e63e9626311e
Jannik Arndt <Jannik@JannikArndt.de>
2014-05-13 18:54:23 +0200

    Split MainWindow > MenuBar.cs now handles all clicks etc.

--- Musicista/MainWindow.xaml.cs
+++ Musicista/MainWindow.xaml.cs
@@ -20,11 +20,7 @@ namespace Musicista
     public partial class MainWindow
     {
         private static List<Canvas> _pageList;
-        public static SidebarInformation SidebarInformation;
-        public static SidebarView SidebarView;
-        public static SidebarAlgorithms SidebarAlgorithms;
         private Piece _currentPiece;
-        private SidebarKind _sidebarKind;
 
         public MainWindow()
         {
@@ -35,7 +31,6 @@ namespace Musicista
             SidebarView = new SidebarView();
             SidebarAlgorithms = new SidebarAlgorithms();
             Sidebar.Content = SidebarInformation;
-            _sidebarKind = SidebarKind.Information;
             ButtonPathInformation.Fill = Brushes.DodgerBlue;
 
             var serializer = new XmlSerializer(typeof(ScorePartwise));
@@ -43,62 +38,6 @@ namespace Musicista
             {
                 var result = (ScorePartwise)serializer.Deserialize(fileStream);
                 DrawPiece(Mapper.MapMusicXMLPartwiseToMusicistaPiece(result));
-            }
-        }
-
-        private void ShowCollection(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void SelectToolAdd(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void SelectToolEdit(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void SelectToolSelect(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void ShowSidebarInformation(object sender, RoutedEventArgs e)
-        {
-            Sidebar.Content = SidebarInformation;
-            SetSidebarButtonPathFill(SidebarKind.Information);
-        }
-
-        private void ShowSidebarView(object sender, RoutedEventArgs e)
-        {
-            Sidebar.Content = SidebarView;
-            SetSidebarButtonPathFill(SidebarKind.View);
-        }
-
-        private void ShowSidebarAlgorithms(object sender, RoutedEventArgs e)
-        {
-            Sidebar.Content = SidebarAlgorithms;
-            SetSidebarButtonPathFill(SidebarKind.Algorithms);
-        }
-
-        private void SetSidebarButtonPathFill(SidebarKind selected)
-        {
-            switch (selected)
-            {
-                case SidebarKind.Information:
-                    ButtonPathInformation.Fill = Brushes.DodgerBlue;
-                    ButtonPathView.Fill = Brushes.Black;
-                    ButtonPathAlgorithms.Fill = Brushes.Black;
-                    break;
-                case SidebarKind.View:
-                    ButtonPathInformation.Fill = Brushes.Black;
-                    ButtonPathView.Fill = Brushes.DodgerBlue;
-                    ButtonPathAlgorithms.Fill = Brushes.Black;
-                    break;
-                case SidebarKind.Algorithms:
-                    ButtonPathInformation.Fill = Brushes.Black;
-                    ButtonPathView.Fill = Brushes.Black;
-                    ButtonPathAlgorithms.Fill = Brushes.DodgerBlue;
-                    break;
             }
         }
 
---
+++ Musicista/MenuBar.cs
@@ -0,0 +1,69 @@
+﻿using Musicista.Sidebar;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Musicista
+{
+    public partial class MainWindow
+    {
+        public static SidebarInformation SidebarInformation;
+        public static SidebarView SidebarView;
+        public static SidebarAlgorithms SidebarAlgorithms;
+
+        private void ShowCollection(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void SelectToolAdd(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void SelectToolEdit(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void SelectToolSelect(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ShowSidebarInformation(object sender, RoutedEventArgs e)
+        {
+            Sidebar.Content = SidebarInformation;
+            SetSidebarButtonPathFill(SidebarKind.Information);
+        }
+
+        private void ShowSidebarView(object sender, RoutedEventArgs e)
+        {
+            Sidebar.Content = SidebarView;
+            SetSidebarButtonPathFill(SidebarKind.View);
+        }
+
+        private void ShowSidebarAlgorithms(object sender, RoutedEventArgs e)
+        {
+            Sidebar.Content = SidebarAlgorithms;
+            SetSidebarButtonPathFill(SidebarKind.Algorithms);
+        }
+
+        private void SetSidebarButtonPathFill(SidebarKind selected)
+        {
+            switch (selected)
+            {
+                case SidebarKind.Information:
+                    ButtonPathInformation.Fill = Brushes.DodgerBlue;
+                    ButtonPathView.Fill = Brushes.Black;
+                    ButtonPathAlgorithms.Fill = Brushes.Black;
+                    break;
+                case SidebarKind.View:
+                    ButtonPathInformation.Fill = Brushes.Black;
+                    ButtonPathView.Fill = Brushes.DodgerBlue;
+                    ButtonPathAlgorithms.Fill = Brushes.Black;
+                    break;
+                case SidebarKind.Algorithms:
+                    ButtonPathInformation.Fill = Brushes.Black;
+                    ButtonPathView.Fill = Brushes.Black;
+                    ButtonPathAlgorithms.Fill = Brushes.DodgerBlue;
+                    break;
+            }
+        }
+    }
+}

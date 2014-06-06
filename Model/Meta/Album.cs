f4c7dd4a348840f6b951152e4586eafadf79a665
Jannik Arndt <Jannik@JannikArndt.de>
2014-06-06 11:11:15 +0200

    Additional album properties

--- Model/Meta/Album.cs
+++ Model/Meta/Album.cs
@@ -1,9 +1,14 @@
 ﻿
+using System;
+
 namespace Model.Meta
 {
     public class Album
     {
         public string Title { get; set; }
+        public string Label { get; set; }
+        public DateTime Published { get; set; }
+        public TimeSpan Duration { get; set; }
 
         public Album() { }
     }

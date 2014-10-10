95379097a5560bd3b5cea8daf67f1872bfeaee97
Jannik Arndt <Jannik@JannikArndt.de>
2014-10-10 17:44:40 +0200

    Bugfix: Files can only be saved if a piece is loaded

--- Musicista/FileOperations.cs
+++ Musicista/FileOperations.cs
@@ -51,6 +51,8 @@ namespace Musicista
 
         public static void SaveFile(string filename, Piece piece)
         {
+            if (String.IsNullOrEmpty(filename))
+                return;
             var serializer = new XmlSerializer(typeof(Piece));
             using (TextWriter writer = new StreamWriter(filename))
             {

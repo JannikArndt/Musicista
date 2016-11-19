49a7a620941616d8f51e9773239c0d309e01b966
Jannik Arndt <jannik@jannikarndt.de>
2016-11-19 12:59:32 +0100

    Added hugo-site

---
+++ README.md
@@ -0,0 +1,5 @@
+# Musicista Hugo Site Branch
+
+This branch keeps the sources for http://jannikarndt.github.io/Musicista/
+
+Pages are generated into the `public` folder, which is a git submodule to the `gh-pages` branch.
---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+git fetch
+if [[ "$(git rev-parse HEAD)" != "$(git rev-parse @{u})" ]]
+	then echo "\033[31mCurrent branch is not up-to-date, please pull first!\033[0m"
+	exit
+fi
+
+echo "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+echo "\033[2m"
+# Build the project.
+hugooutput="$(hugo)"
+echo "$hugooutput"
+echo "\033[0m"
+
+if echo "$hugooutput" | grep "ERROR"
+  then echo "\033[31mError during build, cancelling deployment. 🙁\033[0m"
+  exit
+fi
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+if [ $# -eq 1 ]
+    then msg="$1"
+else
+    echo "\033[1mCommit message: "
+    read "newmsg"
+    echo "\033[0m"
+    msg="$newmsg"
+fi
+
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+git subtree push --prefix=public git@github.com:JannikArndt/Musicista.git gh-pages
+echo "New version is deployed 👍"
